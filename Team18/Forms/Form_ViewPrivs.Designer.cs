baf9b175182b9067ccb2a0b4108d2c86a7e79a15
ntthuan060102github <ntthuan060102.work@gmail.com>
2023-04-06 13:58:35 +0700

    handle view privs form

--- Team18/Forms/Form_ViewPrivs.Designer.cs
+++ Team18/Forms/Form_ViewPrivs.Designer.cs
@@ -28,19 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.view_privs__grantee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.view_privs__object_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.view_privs__column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.view_privs___priv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.view_privs__grantable = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.view_privs__type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.view_privs__grantee,
+            this.view_privs__object_name,
+            this.view_privs__column,
+            this.view_privs___priv,
+            this.view_privs__grantable,
+            this.view_privs__type});
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(801, 451);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // view_privs__grantee
+            // 
+            this.view_privs__grantee.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.view_privs__grantee.HeaderText = "Grantee";
+            this.view_privs__grantee.MinimumWidth = 6;
+            this.view_privs__grantee.Name = "view_privs__grantee";
+            this.view_privs__grantee.ReadOnly = true;
+            // 
+            // view_privs__object_name
+            // 
+            this.view_privs__object_name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.view_privs__object_name.HeaderText = "Object Name";
+            this.view_privs__object_name.MinimumWidth = 6;
+            this.view_privs__object_name.Name = "view_privs__object_name";
+            this.view_privs__object_name.ReadOnly = true;
+            // 
+            // view_privs__column
+            // 
+            this.view_privs__column.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.view_privs__column.HeaderText = "Column";
+            this.view_privs__column.MinimumWidth = 6;
+            this.view_privs__column.Name = "view_privs__column";
+            this.view_privs__column.ReadOnly = true;
+            // 
+            // view_privs___priv
+            // 
+            this.view_privs___priv.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.view_privs___priv.HeaderText = "Privilege";
+            this.view_privs___priv.MinimumWidth = 6;
+            this.view_privs___priv.Name = "view_privs___priv";
+            this.view_privs___priv.ReadOnly = true;
+            // 
+            // view_privs__grantable
+            // 
+            this.view_privs__grantable.HeaderText = "Grantable";
+            this.view_privs__grantable.MinimumWidth = 6;
+            this.view_privs__grantable.Name = "view_privs__grantable";
+            this.view_privs__grantable.ReadOnly = true;
+            this.view_privs__grantable.Width = 125;
+            // 
+            // view_privs__type
+            // 
+            this.view_privs__type.HeaderText = "Type";
+            this.view_privs__type.MinimumWidth = 6;
+            this.view_privs__type.Name = "view_privs__type";
+            this.view_privs__type.ReadOnly = true;
+            this.view_privs__type.Width = 125;
             // 
             // Form_ViewPrivs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form_ViewPrivs";
             this.Text = "Quyền của user/role";
+            this.Load += new System.EventHandler(this.Form_ViewPrivs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn view_privs__grantee;
+        private System.Windows.Forms.DataGridViewTextBoxColumn view_privs__object_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn view_privs__column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn view_privs___priv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn view_privs__grantable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn view_privs__type;
     }
 }
--- Team18/Forms/Form_ViewPrivs.cs
+++ Team18/Forms/Form_ViewPrivs.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tutorial.SqlConn;
 
 namespace Team18.Forms
 {
@@ -15,6 +16,19 @@ namespace Team18.Forms
         public Form_ViewPrivs()
         {
             InitializeComponent();
+            DataTable dt = new DataTable();
+            DBOracleUtils.ExecProc_OutputDataTable("SP_VIEW_LIST_PRIVILEGE", ref dt);
+            if (dt.Rows.Count <= 0) return;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                this.dataGridView1.Rows.Add(dr["GRANTEE"], dr["OBJECT_NAME"], dr["COLUMN"], dr["PRIVILEGE"], dr["TYPE"], dr["GRANTABLE"], dr["TYPE"]);
+            }
+        }
+
+        private void Form_ViewPrivs_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
