b9107335ebe9bf2303e1d4ec9a7196cec5a85f5c
ntthuan060102github <ntthuan060102.work@gmail.com>
2023-04-07 22:40:29 +0700

    update procedure SP_VIEW_USER_PRIVILEGE

--- Team18/Forms/Form_CRUDuser_role.Designer.cs
+++ Team18/Forms/Form_CRUDuser_role.Designer.cs
@@ -37,30 +37,33 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(586, 199);
+            this.textBox1.Location = new System.Drawing.Point(243, 41);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 30);
+            this.textBox1.Size = new System.Drawing.Size(293, 35);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(586, 257);
+            this.textBox2.Location = new System.Drawing.Point(243, 107);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(200, 30);
+            this.textBox2.Size = new System.Drawing.Size(293, 35);
             this.textBox2.TabIndex = 1;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(505, 205);
+            this.label1.Location = new System.Drawing.Point(246, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 17);
             this.label1.TabIndex = 2;
@@ -70,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(506, 262);
+            this.label2.Location = new System.Drawing.Point(246, 87);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 3;
@@ -78,27 +81,27 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(612, 293);
+            this.button1.Location = new System.Drawing.Point(316, 148);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 30);
             this.button1.TabIndex = 4;
-            this.button1.Text = "create";
+            this.button1.Text = "create account";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(586, 371);
+            this.textBox3.Location = new System.Drawing.Point(243, 252);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(200, 30);
+            this.textBox3.Size = new System.Drawing.Size(293, 38);
             this.textBox3.TabIndex = 5;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(534, 377);
+            this.label3.Location = new System.Drawing.Point(246, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 17);
             this.label3.TabIndex = 6;
@@ -107,17 +110,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(612, 408);
+            this.button2.Location = new System.Drawing.Point(314, 296);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(143, 30);
             this.button2.TabIndex = 7;
-            this.button2.Text = "create";
+            this.button2.Text = "create role";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(612, 329);
+            this.button3.Location = new System.Drawing.Point(316, 184);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(143, 30);
             this.button3.TabIndex = 8;
@@ -125,11 +128,40 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(243, 347);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(0, 0);
+            this.textBox4.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(243, 353);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(293, 24);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "users/roles";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(314, 383);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 30);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "remove";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form_CRUDuser_role
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
@@ -157,5 +189,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
